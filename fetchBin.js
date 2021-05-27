36cd46a6b21d8bd6bf2c4bf2a3c4389836219152
Taisuke Fukuno <fukuno@jig.jp>
2021-05-27 14:28:06 +0900

    update

--- fetchBin.js
+++ fetchBin.js
@@ -1,14 +1,14 @@
 const fetchViaProxy = (url) => {
-    const host = "fukuno.jig.jp";
-	const url_proxy = "https://" + host + "/proxy/ITqT5WkhCf2yn1s9?url=" + encodeURI(url);
-    return fetch(url_proxy);
+  //const urlProxy = "https://fukuno.jig.jp/proxy/ITqT5WkhCf2yn1s9?url=" + encodeURIComponent(url);
+  const urlProxy = "http://localhost:8008/?url=" + encodeURIComponent(url);
+  return fetch(urlProxy);
 };
 
 const fetchBin = async (url) => {
-    const flg = url.startsWith("https://") || url.startsWith("http://");
-    const f = flg ? fetchViaProxy : fetch;
-    console.log(flg, url)
-    return new Uint8Array(await (await f(url)).arrayBuffer());
+  const flg = url.startsWith("https://") || url.startsWith("http://");
+  const f = flg ? fetchViaProxy : fetch;
+  console.log(flg, url);
+  return new Uint8Array(await (await f(url)).arrayBuffer());
 };
 
 export { fetchBin };
--- fetch_save.js
+++ fetch_save.js
@@ -1,13 +1,18 @@
-import { GtfsRealtimeApi } from "https://taisukef.github.io/gtfs-realtime-bindings/deno/gtfs-realtime.js";
-
 const list = [
-    { url: "http://www3.unobus.co.jp/GTFS/GTFS_RT-VP.bin", fn: "unobus.pb" },
-    { url: "https://files-skybrain.ekispert.jp/toyama/gtfs-rt/latest/chitetsu/TripUpdates.pb", fn: "toyamabus.pb" },
-    { url: "https://files-skybrain.ekispert.jp/toyama/gtfs-rt/latest/nanto-city/VehiclePositions.pb", fn: "nantobus.pb", },
+  { url: "http://www3.unobus.co.jp/GTFS/GTFS_RT-VP.bin", fn: "unobus.pb" },
+  {
+    url:
+      "https://files-skybrain.ekispert.jp/toyama/gtfs-rt/latest/chitetsu/TripUpdates.pb",
+    fn: "toyamabus.pb",
+  },
+  {
+    url:
+      "https://files-skybrain.ekispert.jp/toyama/gtfs-rt/latest/nanto-city/VehiclePositions.pb",
+    fn: "nantobus.pb",
+  },
 ];
 
 for (const d of list) {
-    const data = new Uint8Array(await (await fetch(d.url)).arrayBuffer());
-    await Deno.writeFile(d.fn, data);
+  const data = new Uint8Array(await (await fetch(d.url)).arrayBuffer());
+  await Deno.writeFile(d.fn, data);
 }
-
